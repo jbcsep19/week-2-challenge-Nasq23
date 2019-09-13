c17db6ce7f4d442a3257ab94915be6aae2b41aca
GBTC441009UR <GBTC441009UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-13 09:35:19 -0400

    week2challenge

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int ricer, beansr, meatr, salsar, veggiesr;
+
+
+        String[] burritos = new String[10
+                ];
+
+        //Make these options versitile but random
+
+
+        String[] rice = {"white rice", "brown rice", "no rice", "all rice options"};
+        String[] meat = {"chicken", "steak", "carnidas", "chorizo", "sofirtas", "veggies", "no meat",  "all meat options"};
+        String[] beans = {"pinto beans", "black beans", "no beans"};
+        String[] salsa = {"mild salsa", "mediums alsa", "hot salsa", "no salsa", "all salsa options"};
+        String[] veggies = {"lettuce", "fajita veggies", "no veggies", "all veggie options"};
+
+
+        //Choose these option in random form for each
+
+
+        for (int i = 0; i < burritos.length; i++) {
+
+
+            ricer = random.nextInt(rice.length);
+            meatr = random.nextInt(meat.length);
+            beansr = random.nextInt(beans.length);
+            salsar= random.nextInt(salsa.length);
+            veggiesr = random.nextInt(veggies.length);
+
+            burritos[i] = "Burrito " + (i + 1) + ": " + rice[ricer] + ", " + meat[meatr] + ", " + beans[beansr] + ", " + salsa[salsar] + ", " + veggies[veggiesr];
+
+            System.out.println(burritos[i ]);
+
+        }
+    }
+
+
+}
+
